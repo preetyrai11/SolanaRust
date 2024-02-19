5f4c19bee7f0a97b1e06500f5d3271f6785853a4
Preety Rai <drpreetyrai@gmail.com>
2024-02-19 21:18:42 +0530

    modifications

--- frontend/components/header/Action.js
+++ frontend/components/header/Action.js
@@ -18,11 +18,11 @@ const Action = ({ setModalOpen }) => {
                 <span className="font-medium text-white">Pay</span>
             </button>
         </div>
-        <div>
-            <button onClick={onDeposit} className="w-full rounded-lg bg-[#1e0f24] py-3  my-5 hover:bg-opacity-70">
-                <span className="font-medium text-white">Deposit</span>
-            </button>
-        </div>
+        // <div>
+        //     <button onClick={onDeposit} className="w-full rounded-lg bg-[#1e0f24] py-3  my-5 hover:bg-opacity-70">
+        //         <span className="font-medium text-white">Deposit</span>
+        //     </button>
+        // </div>
         <div>
             <button onClick={onWithdraw} className="w-full rounded-lg bg-[#1e0f24] py-3 my-5 hover:bg-opacity-70">
                 <span className="font-medium text-white">Withdraw</span>
